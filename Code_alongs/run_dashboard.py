38d4585c2ffb08fbaa9c618e93c2063675d05dc2
Richard Bodén <utjis@hotmail.com>
2024-09-05 10:52:14 +0200

    css and dashboard code along

--- Code_alongs/run_dashboard.py
+++ Code_alongs/run_dashboard.py
@@ -2,6 +2,6 @@ import subprocess
 from pathlib import Path
 
 if __name__ == "__main__":
-    dashboard_path = Path(__file__).parent / "03_streamlit.py"
+    dashboard_path = Path(__file__).parent / "dashboard.py"
     subprocess.run(f"streamlit run {dashboard_path}", shell=True) # öppnar en terminal och kör kommandot
     
